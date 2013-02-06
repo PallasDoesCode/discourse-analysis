f9c163b90ae9c29f45a01c053bc7e627c0cbf1da
Justin R. Hurst <Jryanhurst@gmail.com>
2013-02-06 10:26:38 -0600

    Added in the form and php code for login.php

--- login.php
+++ login.php
@@ -1,10 +1,57 @@
+<?php 
+
+class Login
+{
+	var $userName = "";
+	var $password = "";
+	var $isError = false;
+	
+	function grabFromSubmit($uName, $pWord)
+	{
+		$this->userName = $uName;
+		$this->password = hash('sha256',$pWord);
+		return;
+	}
+	
+};
+
+
+?>
+
 <html>
 <head>
-	<title></title>
+	<title> Discourse Analysis - Login Page </title>
 	
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
-<body>
 
-</body>
+<body>
+<?php
+$login = new Login;
+
+
+if (isset($_POST['username']))
+{
+	$login->grabFromSubmit($_POST['username'], $_POST["password"]);
+
+	$login->showInformation();
+}
+	
+?>		
+<form class="loginForm" name="TestForm" method="post" action="">
+	<p class="field">
+		<input type="text" name="username"  placeholder="Username" />
+	</p>
+	<p class="field">
+		<input type="password" name="password" placeholder="Password" / > 
+	</p>
+	<p class="submit">
+			<input type="submit" name="button" id="button" value="Submit" />
+	</p>
+</form>
+
+
+</body>	
+
 
 </html>

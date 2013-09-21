d05da8fe4d9e14cc34dfffdf0fb5328f438bdb6d
olinic <otter012@Olivers-MacBook-Air.local>
2013-09-20 21:59:54 -0500

    Additions to FileModule.php

--- fileModule.php
+++ fileModule.php
@@ -4,9 +4,7 @@
 	class FileModule
 	{
 		private $dbConnection;
-		private $file;
-		private $fileName;
-		private $user;
+
 		
 		function FileModule($connection)
 		{
@@ -14,12 +12,56 @@
 		}
 		
 		//upload a file based on username
-		
+		function upload($userName, $fileName, $file, $public) {
+			
+		}
 		
 		//get files' information based on username, return array
+		function getFilesInfo($username) {
 		
+		}
+		
+		//get public files' information
+		function getPublicFilesInfo() {
+		
+		}
 		
 		//get file contents based on username and filename
+		function getFileContents($userName, $fileName) {
+		
+		}
+		
+		//internal function, check if username is valid
+		function validUserName($userName) {
+			if($stmt = $this->dbConnection->prepare("SELECT COUNT(Username)
+													FROM usersinfo
+													WHERE Username = ?");
+			$stmt->bind_param("s", $userName);
+			$stmt->execute();
+			$stmt->bind_result($userNameCount);
+			$stmt->fetch();
+			$stmt->close();
+			if ($userNameCount != 0) { //the username must exist
+				return true;
+			}
+			else return false;
+		}
+		
+		//internal function, check if filename exists under the username
+		function fileExists($userName, $fileName) {
+			if($stmt = $this->dbConnection->prepare("SELECT COUNT(fileName)
+													FROM files
+													WHERE Owner = ? AND fileName = ?");
+			$stmt->bind_param("ss", $userName, $fileName);
+			$stmt->execute();
+			$stmt->bind_result($fileCount);
+			$stmt->fetch();
+			$stmt->close();
+			if ($fileCount != 0) { //the file must exist
+				return true;
+			}
+			else return false;	
+		}
 	}
 
 ?>

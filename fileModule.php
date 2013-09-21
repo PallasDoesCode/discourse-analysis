dbe9ff87de70db347c829a6f1b8eb2450b74cd20
olinic <otter012@Olivers-MacBook-Air.local>
2013-09-20 21:23:49 -0500

    Added the file module for the database uploading and downloading of files.

---
+++ fileModule.php
@@ -0,0 +1,25 @@
+<?php
+	//This is the database file module for uploading and downloading user files
+	
+	class FileModule
+	{
+		private $dbConnection;
+		private $file;
+		private $fileName;
+		private $user;
+		
+		function FileModule($connection)
+		{
+			$this->dbConnection = $connection; //use the connection that was given to me
+		}
+		
+		//upload a file based on username
+		
+		
+		//get files' information based on username, return array
+		
+		
+		//get file contents based on username and filename
+	}
+
+?>

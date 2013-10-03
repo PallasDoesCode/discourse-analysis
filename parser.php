6b990e52e4af339971918731b357279c502f491b
olinic <otter012@Olivers-MacBook-Air.local>
2013-10-03 08:09:21 -0500

    Parser Update

--- parser.php
+++ parser.php
@@ -4,34 +4,39 @@
 	class Parser
 	{
 		private $pconjList;
-		
+		private $defaultPconjList = array('WHEN','WHENEVER','WHILE','AS','AS LONG AS','AS SOON AS','SINCE','UNTIL','JUST AS','AT THE SAME TIME AS','WHERE','WHEREVER','THEREFORE','AS A RESULT','FOR THIS REASON','CONSEQUENTLY','HENCE','ACCORDINGLY','THUS','SO','BECAUSE','FOR','SINCE','IN AS MUCH AS','IN ORDER THAT','SO THAT','THAT','TO THE END THAT','FOR THE PURPOSE THAT','LEST','THUS','IN THIS MANNER','IN THAT MANNER','BY THIS MEANS','BY THAT MEANS','SUCH THAT','IF','ONLY IF','UNLESS','EXCEPT THAT','EXCEPT IF' ,'ALTHOUGH','THOUGH','EVEN THOUGH','EVEN IF','X','AND','NOW','BUT','ALSO','OR','WHETHER','TILL','THEN','NEVERTHELESS','YET','STILL','ONLY','ON THE OTHER HAND','CONVERSELY','ON THE CONTRARY','INSTEAD','NOTWITHSTANDING','NOR','LIKEWISE','EITHER','ELSE','OR ELSE','MOREOVER','NEITHER','THAN','INDEED','OTHERWISE','INASMUCH AS');
+				
+				
 				
 		// !We need to consider how to receive the list of pconj's (potential conjunctions) --> (array, string, csv, etc)
-		function Parser($listOfPconj = array()) { //this uses a default parameter -> it does not have to be used
+		function Parser($listOfPconj = "useDefault") { //this uses a default parameter -> it does not have to be used
 												// ex. $myParser = new Parser(); OR $myParser = new Parser($myArray);
-			$this->pconjList = $listOfPconj;
+			if($listOfPconj == "useDefault") {
+				$listOfPconj = $this->defaultPconjList;
+			}
 			
+			$this->pconjList = $listOfPconj;
+
 		}
 		
 		//parse unformatted text
 		function parseUnformattedText($inputText) {
 			//create book, clause, text nodes
-			
-			
-			//get text with pconj's
-			$text = $this->textWithPconj($inputText);			
-			
-			//put text with pconj's into text node
-
+			$book = new SimpleXmlElement("<book></book>");
+			$clause = $book->addChild("clause");
+			$text = $clause->addChild("text", $inputText);
+			$text->addAttribute("chapter", 1);
+			$text->addAttribute("verse", 1);
 			
 			//make list of pconj's for beginning of file
-			$pconjs = getPconjList();
+			$pconjs = $this->getPconjList();
 			
 			//convert the xml to string
-			
+			$xml = $book->asXml();			
 			
 			//combine the list of pconj's and xml string
-			
+			$xml = "$pconjs\n$xml";
+			return $xml;
 		}
 		
 		
@@ -44,50 +49,21 @@
 		function parseFormattedText() {
 		
 		}
-		
-		//get text with pconj's
-		function textWithPconj($text) {
-			
-			
-			
-		}
+
 		
 		//make the list of pconj's for the beginning of file
 		//returns a string of pconj's in xml format
 		function getPconjList() {
 			//use $this->pconjList
-			String $pconjXml = '<pconj>';
-			for($i = 1; $i < $pconjList.length(); $i++) {
 			
-				if($pconjXml.length() < 8) {
-				
-					$pconjXml = $pconjXml + $pconjList[i].explode() + '</pconj>';
-					
-				}
-				else {
-				
-					$pconjXml = $pconjXml + '<pconj>' + $pconjList[i].explode() + '</pconj>';
-				
-				}
-			
-			}
-			
+			$pconjXml = '<pconj>';
+			$xmlInside = implode("</pconj>\n<pconj>", $this->pconjList);
+			$pconjXml = "$pconjXml$xmlInside</pconj>\n";
 			return $pconjXml;
 			
 		}
 		
-		function setPconjList($inputText) {
-		
-			//receive a custom conjunction list
-		
-		}
-		
-		function setPconjList() {
-		
-			//use the default conjunction list
-			$pconjList = new array['WHEN','WHENEVER','WHILE','AS','AS LONG AS','AS SOON AS','SINCE','UNTIL','JUST AS','AT THE SAME TIME AS','WHERE','WHEREVER','THEREFORE','AS A RESULT','FOR THIS REASON','CONSEQUENTLY','HENCE','ACCORDINGLY','THUS','SO','BECAUSE','FOR','SINCE','IN AS MUCH AS','IN ORDER THAT','SO THAT','THAT','TO THE END THAT','FOR THE PURPOSE THAT','LEST','THUS','IN THIS MANNER','IN THAT MANNER','BY THIS MEANS','BY THAT MEANS','SUCH THAT','IF','ONLY IF','UNLESS','EXCEPT THAT','EXCEPT IF' ,'ALTHOUGH','THOUGH','EVEN THOUGH','EVEN IF','X','AND','NOW','BUT','ALSO','OR','WHETHER','TILL','THEN','NEVERTHELESS','YET','STILL','ONLY','ON THE OTHER HAND','CONVERSELY','ON THE CONTRARY','INSTEAD','NOTWITHSTANDING','NOR','LIKEWISE','EITHER','ELSE','OR ELSE','MOREOVER','NEITHER','THAN','INDEED','OTHERWISE','INASMUCH AS'];
-		
-		}
+
 		
 	
 	}

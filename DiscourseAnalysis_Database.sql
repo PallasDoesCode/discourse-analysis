31dc97e1d4153d54f9a8a4bfcf3589826d32efe4
Tyler Hughes <thughes@randomlyknighted.com>
2014-11-11 10:50:03 -0600

    Updated the database

--- DiscourseAnalysis_Database.sql
+++ DiscourseAnalysis_Database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2014 at 12:46 AM
+-- Generation Time: Nov 11, 2014 at 05:48 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `discourseanalysis`
 --
-CREATE DATABASE IF NOT EXISTS `discourseanalysis` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `discourseanalysis`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +26,6 @@ USE `discourseanalysis`;
 -- Table structure for table `conjunctions`
 --
 
-DROP TABLE IF EXISTS `conjunctions`;
 CREATE TABLE IF NOT EXISTS `conjunctions` (
   `conjunction` varchar(25) NOT NULL,
   `listName` varchar(50) NOT NULL
@@ -118,7 +115,6 @@ INSERT INTO `conjunctions` (`conjunction`, `listName`) VALUES
 -- Table structure for table `files`
 --
 
-DROP TABLE IF EXISTS `files`;
 CREATE TABLE IF NOT EXISTS `files` (
   `owner` varchar(40) NOT NULL,
   `projectName` varchar(35) NOT NULL,
@@ -127,12 +123,6 @@ CREATE TABLE IF NOT EXISTS `files` (
   `public` tinyint(1) NOT NULL DEFAULT '0',
   `lastUpdate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELATIONS FOR TABLE `files`:
---   `owner`
---       `usersinfo` -> `username`
---
 
 --
 -- Dumping data for table `files`
@@ -149,7 +139,6 @@ INSERT INTO `files` (`owner`, `projectName`, `fileName`, `storedFileName`, `publ
 -- Table structure for table `permission`
 --
 
-DROP TABLE IF EXISTS `permission`;
 CREATE TABLE IF NOT EXISTS `permission` (
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   `username` varchar(40) NOT NULL,
@@ -166,13 +155,12 @@ CREATE TABLE IF NOT EXISTS `permission` (
 -- Table structure for table `session`
 --
 
-DROP TABLE IF EXISTS `session`;
 CREATE TABLE IF NOT EXISTS `session` (
   `username` varchar(40) NOT NULL,
   `startTime` datetime NOT NULL,
   `endtime` datetime NOT NULL,
 `sessionID` int(15) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
 
 --
 -- Dumping data for table `session`
@@ -182,11 +170,22 @@ INSERT INTO `session` (`username`, `startTime`, `endtime`, `sessionID`) VALUES
 ('user', '2014-10-04 21:39:45', '2014-10-04 21:39:47', 1),
 ('user', '2014-10-20 22:16:34', '2014-10-20 22:19:05', 2),
 ('admin', '2014-10-20 22:19:22', '2014-10-20 22:19:44', 3),
-('user', '2014-10-20 22:20:13', '2014-10-20 22:20:22', 4),
-('user', '2014-10-20 23:06:06', '2014-10-20 23:06:15', 5),
-('user', '2014-10-21 18:02:51', '2014-10-21 18:03:13', 6),
-('admin', '2014-10-23 17:35:18', '2014-10-23 17:38:24', 7),
-('admin', '2014-10-23 17:38:53', '2014-10-23 17:40:55', 8);
+('admin', '2014-10-23 17:35:18', '2014-10-23 17:38:24', 4),
+('admin', '2014-10-23 17:38:53', '2014-10-23 17:40:55', 5),
+('admin', '2014-11-10 22:46:55', '2014-11-10 22:48:06', 29),
+('user', '2014-11-10 22:50:36', '2014-11-10 22:50:39', 30),
+('user', '2014-11-10 22:51:21', '2014-11-10 22:51:29', 31),
+('admin', '2014-11-10 22:51:32', '2014-11-10 22:51:35', 32),
+('admin', '2014-11-10 22:52:18', '2014-11-10 22:52:21', 33),
+('admin', '2014-11-10 22:52:24', '2014-11-10 22:52:27', 34),
+('user', '2014-11-10 22:52:32', '2014-11-10 22:52:34', 35),
+('admin', '2014-11-10 22:54:53', '2014-11-10 22:54:58', 36),
+('admin', '2014-11-10 22:54:53', '2014-11-10 22:55:49', 37),
+('user', '2014-11-10 22:55:57', '2014-11-10 22:55:59', 38),
+('admin', '2014-11-10 22:56:09', '2014-11-10 22:56:22', 39),
+('user', '2014-11-10 22:56:34', '2014-11-10 22:56:41', 40),
+('jdoe', '2014-11-10 22:57:00', '2014-11-10 22:57:07', 41),
+('admin', '2014-11-10 22:59:01', '2014-11-10 22:59:08', 42);
 
 -- --------------------------------------------------------
 
@@ -194,7 +193,6 @@ INSERT INTO `session` (`username`, `startTime`, `endtime`, `sessionID`) VALUES
 -- Table structure for table `tempusersinfo`
 --
 
-DROP TABLE IF EXISTS `tempusersinfo`;
 CREATE TABLE IF NOT EXISTS `tempusersinfo` (
   `confirm_code` varchar(65) NOT NULL,
   `username` varchar(40) NOT NULL,
@@ -210,23 +208,25 @@ CREATE TABLE IF NOT EXISTS `tempusersinfo` (
 -- Table structure for table `usersinfo`
 --
 
-DROP TABLE IF EXISTS `usersinfo`;
 CREATE TABLE IF NOT EXISTS `usersinfo` (
   `username` varchar(40) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(75) NOT NULL,
   `name` varchar(50) NOT NULL,
   `firstName` varchar(25) NOT NULL,
-  `admin` tinyint(1) NOT NULL DEFAULT '0'
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `isOnline` tinyint(1) NOT NULL,
+  `lastLogin` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `usersinfo`
 --
 
-INSERT INTO `usersinfo` (`username`, `password`, `email`, `name`, `firstName`, `admin`) VALUES
-('admin', '$2y$10$/gMa82nHTsYaqCKjvXvmhuVqylwoLLaH6UBLVMuBBPGIDGAf6glxu', 'admin@email.com', 'Administrator', 'Administrator', 1),
-('user', '$2y$10$/gMa82nHTsYaqCKjvXvmhuVqylwoLLaH6UBLVMuBBPGIDGAf6glxu', 'user@gmail.com', 'John Doe', 'John', 0);
+INSERT INTO `usersinfo` (`username`, `password`, `email`, `name`, `firstName`, `admin`, `isOnline`, `lastLogin`) VALUES
+('admin', '$2y$10$/gMa82nHTsYaqCKjvXvmhuVqylwoLLaH6UBLVMuBBPGIDGAf6glxu', 'admin@email.com', 'Administrator', 'Administrator', 1, 1, '2014-11-10 22:59:08'),
+('jdoe', '$2y$10$/gMa82nHTsYaqCKjvXvmhuVqylwoLLaH6UBLVMuBBPGIDGAf6glxu', 'jdoe@email.com', 'Jane Doe', 'Jane', 0, 0, NULL),
+('user', '$2y$10$/gMa82nHTsYaqCKjvXvmhuVqylwoLLaH6UBLVMuBBPGIDGAf6glxu', 'user@gmail.com', 'John Doe', 'John', 0, 0, '2014-11-10 22:56:41');
 
 --
 -- Indexes for dumped tables
@@ -276,7 +276,7 @@ ALTER TABLE `usersinfo`
 -- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
-MODIFY `sessionID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+MODIFY `sessionID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=43;
 --
 -- Constraints for dumped tables
 --
